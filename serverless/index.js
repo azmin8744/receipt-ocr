23b17b17c8793155f0d14913c9e40ae26633dbbf
azmin8744 <azmin8744@gmail.com>
2018-10-09 18:30:59 +0900

    treat file name and bucket name

--- serverless/index.js
+++ serverless/index.js
@@ -2,6 +2,10 @@
 const request = require('request-promise');
 
 exports.event = (event, callback) => {
-  console.log('event triggerd!');
-  callback();
+    console.log('event triggerd!');
+    const file = event.data;
+    const bucket = file.bucket;
+    const gcs_uri = `gs://${file.bucket}/${file.name}`
+    console.log(gcs_uri);
+    callback();
 };
